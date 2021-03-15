27c9f016a85624208ef8699ce65bcb7150116794
SAM <tongyuhui4545@gmail.com>
2021-03-15 21:07:42 +1100

    Add project files.

---
+++ CRUD/Models/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string MessageQuestion { get; set; }
+        public string MessageAnswer { get; set; }
+        public Message()
+        {
+
+        }
+    }
+}
